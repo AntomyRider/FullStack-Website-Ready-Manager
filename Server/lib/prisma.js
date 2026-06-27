5ac9ce67d675ae2d20d89192a903945a32dd5370
NutxV2 <nganyanwainakarin@gmail.com>
2026-06-27 14:37:14 +0700

    fix discord bot

--- Server/lib/prisma.js
+++ Server/lib/prisma.js
@@ -6,6 +6,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DB_USER || process.env.USER || 'root',
   password: process.env.DB_PASSWORD || process.env.PASSWORD || 'bonus2548',
   database: process.env.DB_NAME || process.env.NAME || 'License',
+  allowPublicKeyRetrieval: true,
 })
 
 const prisma = new PrismaClient({ adapter })
